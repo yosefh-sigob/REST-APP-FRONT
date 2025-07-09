4a199b233c15688bfd3c3a266d4a64647d6f940b
FernandoSigob <fernando.camarena@sigob.com.mx>
2025-07-08 23:04:16 -0600

    fix: Se comentó route para evitar el error unused

--- app/api/empresas/[id]/route.ts
+++ app/api/empresas/[id]/route.ts
@@ -1,4 +1,9 @@
+import { NextRequest, NextResponse } from "next/server";
 
+export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+  const { id } = params;
+  return NextResponse.json({ message: `Empresa con ID: ${id}` });
+}
 
 // export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
 //   try {
